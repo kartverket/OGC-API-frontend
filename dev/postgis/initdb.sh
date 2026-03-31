5a22540fa39629adc3c68a5c61efebb855245490
Christian H. Mosveen <christian.mosveen@kartverket.no>
2026-03-31 10:19:24 +0200

    Fjern hacks som ikke trengs for sql-dumpene lenger

--- dev/postgis/initdb.sh
+++ dev/postgis/initdb.sh
@@ -9,15 +9,7 @@ psql -U "$POSTGRES_USER" -c "CREATE DATABASE $db;"
 psql -U "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION postgis;"
 
 for file in "$directory"/*.sql; do
-    # Hack for GiST index
-    sed -i '/USING GIST ("representasjonspunkt")/d' "$file"
-
     psql -U "$POSTGRES_USER" -d "$db" -f "$file"
-
-    # Hack for geom -> geometry column names
-    table=$(basename "$file" .sql)
-    psql -U "$POSTGRES_USER" -d "$db" -c \
-        "ALTER TABLE IF EXISTS \"public\".\"$table\" RENAME COLUMN \"geom\" TO \"geometry\";" 2>/dev/null || true
 done
 
 rm -r $directory
