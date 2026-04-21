7b40df622fa16bf7f4b423bfdb92efaec066ddda
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2026-04-21 10:08:09 +0200

    Removed example use card

--- src/app/collections/[collection]/page.jsx
+++ src/app/collections/[collection]/page.jsx
@@ -12,7 +12,6 @@ import {
   DatasetInfoCard,
   DownloadPanel,
   ErrorPage,
-  ExampleUseCard,
 } from "@/components";
 import { collectionHasMapProvider } from "@/config/readPygeoapiConfig";
 import { fetchCollectionPageData } from "@/services/pageData";
@@ -97,7 +96,6 @@ export default async function Collection({ params }) {
 
               {/* <Link href={geonorgeLink.href} target="_blank" className={styles.geonorgeLink}>Vis datasettet på Geonorge</Link> */}
 
-              <ExampleUseCard collection={collection} hasMap={hasMap} />
               <DownloadPanel collectionId={collection} downloadConfig={data.downloadConfig} />
             </div>
           </div>
