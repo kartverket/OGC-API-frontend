1f1ad36833be322dd41a7a82351393eb33810799
Christian H. Mosveen <christian.mosveen@kartverket.no>
2026-08-12 16:07:57 +0200

    Prøv å få ut litt logg

--- pygeoapi/entrypoint.py
+++ pygeoapi/entrypoint.py
@@ -3,9 +3,10 @@
 # Remove GOOGLE_APPLICATION_CREDENTIALS from the environment to avoid GDAL
 # trying to use it for authentication.
 import os
-print("BEFORE POP:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+print("BEFORE POP:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"), flush=True)
 os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
-print("AFTER POP:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+print("AFTER POP:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"), flush=True)
+print("PID:", os.getpid(), flush=True)
 
 import re
 from modify_openapi import remove_post_endpoints
