69533e71eb13184ce0e56a9879af09bb39941e69
Thomas Ellett von Brasch <Thomas.Ellett@kartverket.no>
2026-08-12 13:48:35 +0200

    Changed table logic for items and item pages

--- src/components/ItemData/index.jsx
+++ src/components/ItemData/index.jsx
@@ -2,8 +2,6 @@ import ItemDataProperty from './ItemDataProperty';
 import styles from './ItemData.module.css';
 
 export default function ItemData({ data }) {
-    const { [data.idField]: _, ...properties } = data.properties;
-
     return (
         <div className={styles.itemData}>
             <div>
@@ -12,12 +10,7 @@ export default function ItemData({ data }) {
                     <div>Verdi</div>
                 </div>
 
-                <div className={styles.row}>
-                    <div className={styles.label}>id</div>
-                    <div className={styles.value}>{data.id}</div>
-                </div>
-
-                <ItemDataProperty data={properties} />
+                <ItemDataProperty data={data.properties} />
             </div>
         </div>
     );
